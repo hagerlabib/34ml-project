e4360968f404ae5d7f2f6088a94d6cab95f9b5de
hager <hageerlabib@gmail.com>
2024-07-26 19:27:26 +0300

    feat: database schema

---
+++ database/migrations/2024_07_25_123938_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Lesson;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->foreignIdFor(Lesson::class);
+            $table->foreignIdFor(User::class);
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
---
+++ database/migrations/2024_07_25_125812_create_watches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Lesson;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('watches', function (Blueprint $table) {
+            $table->foreignIdFor(Lesson::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('watches');
+    }
+};
